61e2745844c092d7facfeea8694cee0ca542b505
Henry Chen <tahafut@gmail.com>
2019-01-29 13:42:35 -0800

    move authentication outside of loop

--- delete_tweets.py
+++ delete_tweets.py
@@ -1,27 +1,29 @@
 import os
 import time
 import twitter
+
 import sentry_sdk
 
 sentry_sdk.init(os.environ["SENTRY_URL"])
 
+api = twitter.Api(
+    consumer_key=os.environ["CONSUMER_KEY"],
+    consumer_secret=os.environ["CONSUMER_SECRET"],
+    access_token_key=os.environ["ACCESS_TOKEN_KEY"],
+    access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
+)
+
+user_id = api.GetUser(screen_name=os.environ["SCREEN_NAME"]).id_str
+
+
 def bot():
-
-    api = twitter.Api(
-        consumer_key=os.environ["CONSUMER_KEY"],
-        consumer_secret=os.environ["CONSUMER_SECRET"],
-        access_token_key=os.environ["ACCESS_TOKEN_KEY"],
-        access_token_secret=os.environ["ACCESS_TOKEN_SECRET"],
-    )
-
-    user_id = api.GetUser(screen_name=os.environ["SCREEN_NAME"]).id_str
-
     for tweet in api.GetStreamFilter(follow=[user_id]):
         statuses = api.GetUserTimeline(user_id=user_id, count=1000, trim_user=True)
 
         for status in statuses[25:]:
             api.DestroyStatus(status.id)
             print(status)
+
 
 while True:
     try:
